fa73c608a90b37e4c8cd405ac7798e32be67983c
Vitaly068 <135343022+Vitaly068@users.noreply.github.com>
2023-08-28 21:24:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def convert(message: telebot.types.Message):
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {base} в {quote} - {total_base}'
+        text = f'Цена {amount} {quote} в {base} - {float(total_base) * float(amount)}'
         bot.send_message(message.chat.id, text)
 
 bot.polling()
